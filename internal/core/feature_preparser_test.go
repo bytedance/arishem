f177691f2faa2c0b00f5df67d14c96d9858d908e
jiangaoran.nekohayo <jiangaoran.nekohayo@bytedance.com>
2023-04-10 15:04:50 +0800

    change copyright
    Change-Id: I2d47df5f3758ca75c735c060679dee4c7217d5de

--- internal/core/feature_preparser_test.go
+++ internal/core/feature_preparser_test.go
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2023 ByteDance Inc.
+ *  Copyright Bytedance Ltd. and/or its affiliates.
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
