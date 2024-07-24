01c907427ae10f96b9659b11f1950c4d7da64809
murasame <jiangaoran.nekohayo@bytedance.com>
2024-07-24 17:12:34 +0800

    change api name
    Change-Id: I824d49d66c6c37db59f228f03ef0b0b8418f63e3

--- arishem/arishem_lw_api.go
+++ arishem/arishem_lw_api.go
@@ -110,7 +110,8 @@ func ParseRuleTree(expr string, exprType ExprType) (exprTree *RuleTree, err erro
 	return
 }
 
-func WalkFeature(featureExpr string) (typedef.FeatureParam, error) {
+// ParseFeature input a feature expression string and return the typedef.FeatureParam
+func ParseFeature(featureExpr string) (typedef.FeatureParam, error) {
 	fppl := core.NewFeaturePreParseListener()
 	err := parser.ParseSingleFeature(featureExpr, fppl)
 	if err != nil {
